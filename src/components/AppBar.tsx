c45ecdaac08b25fa413490671adabc0e7bf1030d
openXapps <32998735+openXapps@users.noreply.github.com>
2025-01-29 15:06:17 +0200

    Progress

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -21,25 +21,23 @@ export default function AppBar() {
   }
 
   return (
-    <div className="bg-slate-400 dark:bg-slate-500">
-      <div className='container'>
-        <div className='container flex justify-between items-center p-3'>
-          <h1 className="text-xl font-bold text-orange-900 dark:text-orange-400">OpenApps Cloud Data Manager</h1>
-          <div className='flex gap-2'>
-            <Button
-              variant="ghost"
-              size="icon"
-              disabled={!isAuthorized}
-              className={twMerge(isAuthorized ? 'text-green-800 dark:text-green-400' : '')}
-              onClick={() => rrNavigate('/user')}
-            ><UserRound />
+    <div className='container max-w-[1024px]'>
+      <div className='flex justify-between items-center p-3 mx-auto'>
+        <h1 className="text-xl font-bold text-orange-900 dark:text-orange-400">OpenApps Cloud Data Manager</h1>
+        <div className='flex gap-2'>
+          <Button
+            variant="ghost"
+            size="icon"
+            disabled={!isAuthorized}
+            className={twMerge(isAuthorized ? 'text-green-800 dark:text-green-400' : '')}
+            onClick={() => rrNavigate('/user')}
+          ><UserRound />
+          </Button>
+          {isAuthorized && (
+            <Button variant="ghost" size="icon" onClick={handleSignUserOut}>
+              <LogOut />
             </Button>
-            {isAuthorized && (
-              <Button variant="ghost" size="icon" onClick={handleSignUserOut}>
-                <LogOut />
-              </Button>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </div>
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -3,11 +3,13 @@ import AppBar from '@/components/AppBar';
 
 export default function Layout() {
   return (
-    <div className="">
-      <AppBar />
-      <div className="container">
+    <>
+      <div className="bg-slate-400 dark:bg-slate-500">
+        <AppBar />
+      </div>
+      <div className="container max-w-[1024px]">
         <Outlet />
       </div>
-    </div>
+    </>
   );
 }
